454337e3debb3287b45d568e89f23ac84550d642
litty1994 <littythomas45@gmail.com>
2024-09-25 11:29:24 +0530

    new

---
+++ src/components/QuoteGenerator1.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {  faQuoteLeft, faQuoteRight } from '@fortawesome/free-solid-svg-icons';
+
+
+const QuoteGenerator1 = () => {
+  const [quote, setQuote] = useState({ quote: '', author: '' });
+
+  // Function to fetch random quote
+  const fetchQuote = async () => {
+    try {
+      const response = await axios.get('https://dummyjson.com/quotes');
+      const randomIndex = Math.floor(Math.random() * response.data.quotes.length);
+      const randomQuote = response.data.quotes[randomIndex];
+      setQuote({ quote: randomQuote.quote, author: randomQuote.author });
+      // setQuote(response.data.quotes[randomIndex].quote);
+    } catch (error) {
+      console.error('Error fetching the quote', error);
+    }
+  };
+
+  // Fetch a quote when component mounts
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', width: '100%', height: '500px', backgroundColor: 'aquamarine' }}>
+      <h1 style={{ fontSize:'40px',color:'blue' }}>Quote of the day</h1>
+      <div  style={{ width: '98%', height: '200px', backgroundColor: 'white',marginLeft:'10px',marginRight:'20px', display:'flex',alignItems:'center',justifyContent:'center'}}>
+       
+            <p style={{ fontSize:'20px', marginTop:'30px' }} ><FontAwesomeIcon icon={ faQuoteLeft} />{quote.quote}<FontAwesomeIcon icon={ faQuoteRight} />
+            </p>
+            
+            <p><br/><br/><strong>- {quote.author}</strong></p>
+
+          </div>
+          
+          
+      <button onClick={fetchQuote}>Next Quote</button>
+    </div>
+  );
+};
+
+export default QuoteGenerator1;
